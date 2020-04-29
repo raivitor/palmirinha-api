8a01d37b7525ea4970832624ef515db95827f09b
Rai Vitor <raivitor@gmail.com>
2020-04-29 09:03:43 -0300

    Added GIPHY_API_KEY

--- src/util/configs.js
+++ src/util/configs.js
@@ -1,4 +1,5 @@
 export const CONFIGS = {
   RECIPE_PUPPY_URL: 'http://www.recipepuppy.com/api',
-  GIPHY_URL: 'http://api.giphy.com/v1/gifs/search'
+  GIPHY_URL: 'http://api.giphy.com/v1/gifs/search',
+  GIPHY_API_KEY: process.env.GIPHY_API_KEY
 };
