5878c542cbef2c6f9a8b4d58e836c081455d0364
Rai Vitor <raivitor@gmail.com>
2020-04-29 09:04:13 -0300

    Added getGif in recipes util

--- src/endpoints/recipes/util.js
+++ src/endpoints/recipes/util.js
@@ -1,3 +1,18 @@
+import axios from 'axios';
+import { CONFIGS } from '../../util/configs';
+
+export async function getGif(title) {
+  const gif = await axios
+    .get(
+      `${CONFIGS.GIPHY_URL}?api_key=${CONFIGS.GIPHY_API_KEY}&q=${title}&limit=1`
+    )
+    .catch(() => {
+      throw 'Giphy API error';
+    });
+
+  return gif.data.data.length === 1 ? gif.data.data[0].url : 'No gif found';
+}
+
 export function sortIngredients(ingredients) {
   return ingredients
     .split(',')
