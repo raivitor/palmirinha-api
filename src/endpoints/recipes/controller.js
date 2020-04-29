ce3317d0091e185e69baab287ef3fc00243ec04b
Rai Vitor <raivitor@gmail.com>
2020-04-29 09:01:29 -0300

    Updated getRecipes to return obj with items

--- src/endpoints/recipes/controller.js
+++ src/endpoints/recipes/controller.js
@@ -1,11 +1,30 @@
+import axios from 'axios';
+import { errorResolver } from '../../util/errorResolver';
+import { CONFIGS } from '../../util/configs';
+import { sortIngredients } from './util';
 
 const getRecipes = async (req, res) => {
   try {
     const i = req.query.i;
 
-    return res.json({ query: i });
+    const puppyReturn = await axios
+      .get(`${CONFIGS.RECIPE_PUPPY_URL}/?i=${i}`)
+      .catch(() => {
+        throw 'RecipePuppy API error';
+      });
+
+    const recipes = await Promise.all(
+      puppyReturn.data.results.map(async item => ({
+        title: item.title.trim(),
+        ingredients: sortIngredients(item.ingredients),
+        link: item.href,
+        gif: item.title
+      }))
+    );
+
+    return res.json({ keywords: sortIngredients(i), recipes });
   } catch (err) {
-    res.status(400).json({ errorMsg: err });
+    errorResolver(err, res);
   }
 };
 
